5b3d4aef4abfb121c4502f18abe81a015ea2826e
Sergio@Mana <sergio@labsma.com>
2023-02-26 13:02:24 -0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/gnr8mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gnr8mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF07FF3E);
+  static const secondary = Color(0xFFADC178);
+  static const accent = Color(0xFFECF0D7);
+  static const back = Color(0xFF232323);
+  static const black = Color.fromARGB(255, 2, 12, 4);
+  static const grey = Color(0xFF3F3F3F);
+}
