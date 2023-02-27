cb1ad5ba697ec3c0e636eedb8c9610d940cc7652
Sergio M. Mahakala <sergio@labsma.com>
2023-02-27 10:06:32 -0700

    migrating away mac

---
+++ lib/utils/theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:gnr8/utils/utils.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData gnr8theme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme(
+    surface: AppColors.black,
+    onSurface: AppColors.back,
+    brightness: Brightness.dark,
+    background: AppColors.black,
+    onBackground: AppColors.grey,
+    primary: AppColors.primary,
+    secondary: AppColors.secondary,
+    onSecondary: Colors.lightGreenAccent,
+    onPrimary: Colors.green,
+    onError: Colors.redAccent,
+    error: Colors.red,
+  ),
+  textTheme: GoogleFonts.montserratTextTheme().copyWith(
+    bodySmall: TextStyle(
+      color: Colors.white,
+    ),
+    bodyMedium: TextStyle(
+      color: Colors.white,
+    ),
+    bodyLarge: TextStyle(
+      color: Colors.white,
+    ),
+    headlineSmall: TextStyle(
+      color: Colors.white,
+    ),
+    headlineMedium: TextStyle(
+      color: Colors.white,
+    ),
+  ),
+);
