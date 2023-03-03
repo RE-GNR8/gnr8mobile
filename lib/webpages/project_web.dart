0ca42841be32eeb149755be4047eed1d93c3dcc2
Sergio M. Mahakala <sergio@labsma.com>
2023-03-03 08:56:43 -0700

    web project

---
+++ lib/webpages/project_web.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProjectWebPage extends StatefulWidget {
+  ProjectWebPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProjectWebPage> createState() => _ProjectWebPageState();
+}
+
+class _ProjectWebPageState extends State<ProjectWebPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
